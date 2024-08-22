4ab9f1912430e1b8ed9fd5584e22d2bcfea71d2f
Cristhian-dlrs <62567122+Cristhian-dlrs@users.noreply.github.com>
2024-08-21 21:03:20 -0400

    feat: add api contracts validation

--- src/TechDemo.Contracts/Permissions/ModifyPermissionRequest.cs
+++ src/TechDemo.Contracts/Permissions/ModifyPermissionRequest.cs
@@ -1,8 +1,25 @@
+using FluentValidation;
+
 namespace TechDemo.Contracts.Permissions;
 
 public record ModifyPermissionRequest(
     int Id,
     string? EmployeeForename,
     string? EmployeeSurname,
-    string? PermissionType
-);
+    string? PermissionType)
+{
+    public const string RouteTemplate = "/permissions/{permissionId:int}";
+    public static string BuildRoute(int permissionId)
+        => RouteTemplate.Replace("{permissionId:int}", permissionId.ToString());
+}
+
+
+public class ModifyPermissionRequestValidator : AbstractValidator<ModifyPermissionRequest>
+{
+    public ModifyPermissionRequestValidator()
+    {
+        RuleFor(x => x.Id)
+            .GreaterThan(0)
+            .WithMessage("Permission id is required.");
+    }
+}
